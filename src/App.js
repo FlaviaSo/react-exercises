7b174bde627794f1caa885cf7e6c43d913088988
FlaviaSoreca <flaviasoreca@gmail.com>
2022-08-30 16:00:57 +0200

    cr-05

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ export class App extends React.Component{
         return (
             <div>
                 <Hello />
-                <Welcome age={27}/>
+                <Welcome age={27
+                }/>
             </div>
             )
     }
