95719e64b7484929a18f9655e737f654acbe6643
FlaviaSoreca <102346681+FlaviaSo@users.noreply.github.com>
2022-09-03 18:02:04 +0200

    fix

--- src/Age.js
+++ src/Age.js
@@ -5,8 +5,10 @@ export class Age extends React.Component{
     render(){
         return (
         <>
-            {this.props.age>=18 && <p>Your age is {this.props.age}</p>}
-            {this.props.age<18 && <p>You are very young!</p>}
+            {this.props.age >= 18
+                    ? <p>Your Age is {this.props.age}</p>
+                    : <p>You are very young!</p>
+            }
         </>
         )
     }
