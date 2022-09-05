87984998caf87bfaa8291d02f1f9eb34d13095f9
FlaviaSoreca <flaviasoreca@gmail.com>
2022-09-05 18:00:06 +0200

    s-02

--- src/Counter.js
+++ src/Counter.js
@@ -25,3 +25,7 @@ export class Counter extends React.Component {
         )
     }
 }
+
+//When calling setState to increment the counter, should the parameter be a function or an object? Why?
+//Può essere sia un oggetto che una funzione, dipende dalle situazioni, utilizzare una funzione è più utile nel momento in cui ci sono più chiamate setState dipendenti l'una dall'altra
+//Questo perché gli state non sono sempre sincroni e quando il valore di uno state dipende dal valore di un altro, per far si che avvengano in maniera sincrona è bene usare le callback al posto degli oggetti
