8aa1d67c396a7ae9090fe762797b50035d7b48bb
FlaviaSoreca <flaviasoreca@gmail.com>
2022-09-07 19:55:20 +0200

    l-06

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,39 +1,49 @@
 import React, { createRef } from "react";
 
 export class TodoList extends React.Component {
-  _inputRef = createRef();
+    _inputRef = createRef();
 
-  state = { todos: ["Voglio", "Mo", "Ri", "Re"] };
+    state = { todos: ["Voglio", "Mo", "Ri", "Re"] };
 
-  handleTodoUpdate = (event) => {
-    const name = event.target.name;
-    const todo = this._inputRef.current.value;
-    const reset = this._inputRef.current.value = "";
+    handleTodoUpdate = (event) => {
+        const name = event.target.name;
+        const todo = this._inputRef.current.value;
+        const reset = this._inputRef.current.value = "";
 
-    this.setState(() => ({
-      [name]: todo !== "" ? this.state.todos.push(todo) : reset,
-    }));
-  };
+        this.setState(() => ({
+            [name]: todo !== "" ? this.state.todos.push(todo) : reset,
+        }));
+    };
 
-  handleResetButton = e =>{
-    this.setState((state) => ({
-        todos: (state.todos = [""])
-    }))
-  }
+    handleResetButton = () => {
+        this.setState((state) => ({
+            todos: (state.todos = [""])
+        }))
+    }
 
-  render() {
+    handleRemoveButton = item => {
+        this.setState(state =>({
+            todos: state.todos.filter(el => el !== item)
+        }))
+    }
+
+
+render() {
     return (
-      <>
-        <ul>
-          {this.state.todos.map((todos, index) => (
-            <li key={todos + index}>{todos}</li>
-          ))}
-        </ul>
+        <>
+            <ul>
+                {this.state.todos.map((todos, index) => (
+                    <li key={index}>
+                        {todos}
+                        <button onClick={() => this.handleRemoveButton(todos)}>Remove</button>
+                    </li>
+                ))}
+            </ul>
 
-        <input ref={this._inputRef}></input>
-        <button onClick={this.handleTodoUpdate}>Aggiungi Chore</button>
-        <button type="reset" onClick={this.handleResetButton}>Reset</button>
-      </>
+            <input ref={this._inputRef}></input>
+            <button onClick={this.handleTodoUpdate}>Aggiungi Chore</button>
+            <button type="reset" onClick={this.handleResetButton}>Reset</button>
+        </>
     );
-  }
+}
 }
