b3dbe0ca4f0c7b5aedf6f40cfbe5d336e7997ab3
FlaviaSoreca <flaviasoreca@gmail.com>
2022-09-12 18:52:55 +0200

    r-p-01

--- src/App.js
+++ src/App.js
@@ -1,44 +1,62 @@
 import React from "react";
-import {Hello} from './Hello';
-import {Welcome} from './Welcome';
-import {Counter} from './Counter'
-import {ClickCounter} from "./ClickCounter";
-import {ClickTracker} from "./ClickTracker"
-import {InteractiveWelcome} from "./InteractiveWelcome"
-import {Login} from "./Login"
+import { Hello } from './Hello';
+import { Welcome } from './Welcome';
+import { Counter } from './Counter'
+import { ClickCounter } from "./ClickCounter";
+import { ClickTracker } from "./ClickTracker"
+import { InteractiveWelcome } from "./InteractiveWelcome"
+import { Login } from "./Login"
 import { UncontrolledLogin } from "./UncontrolledLogin";
-import {Colors} from "./Colors"
-import {TodoList} from "./TodoList";
-import {Container} from "./Container";
-import {Message} from "./Message"
+import { Colors } from "./Colors"
+import { TodoList } from "./TodoList";
+import { Container } from "./Container";
+import { Message } from "./Message"
 
-export class App extends React.Component{
-    render(){
+export class App extends React.Component {
+    render() {
         return (
             <div>
                 <Hello />
-                <Welcome name={"Flavia"} age={27}/>
-                <Counter initialValue={0} incrementBy={1} timeout={1000}/>
-                <ClickCounter /> 
+                <Welcome name={"Flavia"} age={27} />
+                <Counter initialValue={0} incrementBy={1} timeout={1000} />
+                <ClickCounter />
                 <ClickTracker />
                 <div>
-                <br></br>
-                <InteractiveWelcome />
+                    <br></br>
+                    <InteractiveWelcome />
                 </div>
                 <div>
-                <Login />
+                    <Login />
                 </div>
                 <br></br>
                 <div>
-                <UncontrolledLogin />
+                    <UncontrolledLogin />
                 </div>
-                <Colors items={[{id:1,name:'Red'}, {id:2,name:'Green'}, {id:3, name:'Blue'}]}/>
-                <TodoList />
+                <Colors items={[{ id: 1, name: 'Red' }, { id: 2, name: 'Green' }, { id: 3, name: 'Blue' }]} />
+                <TodoList render={({ todos, handleTodoUpdate, handleResetButton, handleRemovebutton }) => {
+                    return(
+                    <div class="border border-danger border-3 p-3 m-2 w-25 d-flex flex-column align-items-center justify-content-center">
+                        <ul class="list-group-flush list-group-numbered">
+                            {todos.map((todos, index) => (
+                                <li key={index} class="d-flex flex-row justify-content-between p-1 list-group-item">
+                                {todos}
+                                <button class="btn btn-danger" onClick={() => this.handleRemoveButton(todos)}>Remove</button>
+                                </li>
+                        ))}
+                    </ul>
+                    <div>
+                        <input class="" ref={this._inputRef}></input>
+                        <button class="btn btn-danger" onClick={this.handleTodoUpdate}>Aggiungi Chore</button>
+                        <button class="btn btn-danger" type="reset" onClick={this.handleResetButton}><span class="aligh-self-center">Reset</span></button>
+                    </div>
+                </div>
+                    )}} />
+
                 <Container title={"Hola, Amigos!"}>
                     <Message />
                 </Container>
             </div>
-            )
+        )
     }
 
 }
--- src/TodoList.js
+++ src/TodoList.js
@@ -17,7 +17,7 @@ export class TodoList extends React.Component {
 
     handleResetButton = () => {
         this.setState((state) => ({
-            todos: (state.todos = [""])
+            todos: (state.todos = [])
         }))
     }
 
@@ -29,21 +29,28 @@ export class TodoList extends React.Component {
 
 
 render() {
-    return (
-        <div class="bg-orange-100 flex flex-col content-center">
-            <ul class="bg-white w-[150px] self-center list-decimal flex flex-col border-2 border-black border-dashed">
+    return this.props.render({
+        todos: this.state.todos,
+        handleTodoUpdate: this.handleTodoUpdate,
+        handleResetButton: this.handleResetButton,
+        handleRemoveButton: this.handleRemoveButton
+    })
+    /*return (
+        <div class="border border-danger border-3 p-3 m-2 w-25 d-flex flex-column align-items-center justify-content-center">
+            <ul class="list-group-flush list-group-numbered">
                 {this.state.todos.map((todos, index) => (
-                    <li key={index} class="font-bold flex justify-between mx-1">
+                    <li key={index} class="d-flex flex-row justify-content-between p-1 list-group-item">
                         {todos}
-                        <button class="w-[60px] bg-red-900 hover:bg-red-500 border font-normal text-xs text-white " onClick={() => this.handleRemoveButton(todos)}>Remove</button>
+                        <button class="btn btn-danger" onClick={() => this.handleRemoveButton(todos)}>Remove</button>
                     </li>
                 ))}
             </ul>
-
-            <input class="w-[150px] self-center border-2 border-black" ref={this._inputRef}></input>
-            <button class="w-[150px] h-[25px] self-center bg-red-900 hover:bg-red-500 border font-normal text-xs text-white" onClick={this.handleTodoUpdate}>Aggiungi Chore</button>
-            <button class="w-[150px] h-[25px] self-center bg-white hover:bg-slate-400 border font-semibold text-lg flex justify-center" type="reset" onClick={this.handleResetButton}><span class="self-center">Reset</span></button>
+            <div>
+            <input class="" ref={this._inputRef}></input>
+            <button class="btn btn-danger" onClick={this.handleTodoUpdate}>Aggiungi Chore</button>
+            <button class="btn btn-danger" type="reset" onClick={this.handleResetButton}><span class="aligh-self-center">Reset</span></button>
+            </div>
         </div>
-    );
+    );*/
 }
 }
